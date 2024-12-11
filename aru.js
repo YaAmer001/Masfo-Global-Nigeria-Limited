330fa0aa2bf169dd5962e57de3de10ec0b62e4cb
Young Programmer <144114469+YaAmer001@users.noreply.github.com>
2024-12-11 20:51:50 +0100

    Add files via upload

---
+++ aru.js
@@ -0,0 +1,5 @@
+// Toggle the hamburger menu
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
